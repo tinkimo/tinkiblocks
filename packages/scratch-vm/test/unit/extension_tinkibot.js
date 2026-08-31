b8663f08bcd0d79eb03b4ad8bd284e5f9b9901b4
Tinkimo <terri@tinkimo.com>
2026-08-31 10:08:18 +0100

    fix(tinkibot): handle proxy command errors

--- packages/scratch-vm/test/unit/extension_tinkibot.js
+++ packages/scratch-vm/test/unit/extension_tinkibot.js
@@ -101,6 +101,36 @@ test('Tinkibot volume command accepts the proxy response and releases the queue'
     delete global.WebSocket;
 });
 
+test('Tinkibot command errors warn the user and release the queue', async t => {
+    global.window = {};
+    global.WebSocket = MockWebSocket;
+    global.alert = message => t.equal(
+        message,
+        'No Tinkibot robots are connected.'
+    );
+
+    const extension = new TinkibotBlocks({});
+    const sound = extension.play_sound({SOUND: 'startup'});
+    const distance = extension.measure_distance();
+
+    MockWebSocket.instance.open();
+    await Promise.resolve();
+    MockWebSocket.instance.respond('error', 'command_failed:no_robots_connected');
+    await t.rejects(sound, {message: 'command_failed:no_robots_connected'});
+    await Promise.resolve();
+    t.strictSame(MockWebSocket.instance.sent, [
+        {command: 'play_sound', sound: 'startup'},
+        {command: 'measure_distance'}
+    ]);
+
+    MockWebSocket.instance.respond('measure_distance', 42);
+    t.equal(await distance, 42);
+
+    delete global.alert;
+    delete global.window;
+    delete global.WebSocket;
+});
+
 test('Tinkibot button events start event hats and filter by their menus', t => {
     global.window = {};
     global.WebSocket = MockWebSocket;
